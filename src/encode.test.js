112a223ffbd93eba4818ca3c5766c60bbb5630c5
Skúli Arnlaugsson <arnlaugsson@users.noreply.github.com>
2018-09-22 09:01:11 +0000

    Add test cases for unicode strings

--- src/encode.test.js
+++ src/encode.test.js
@@ -7,13 +7,16 @@ describe("encode function", () => {
 
   it("should return the same character when given one character", () => {
     expect(encode("0")).toBe("0");
+    expect(encode("Ð")).toBe("Ð");
   });
 
   it("should swap first and second characters when given two", () => {
     expect(encode("XY")).toBe("YX");
+    expect(encode("ÆÞ")).toBe("ÞÆ");
   });
 
   it("should return the same word once given a palindrome", () => {
     expect(encode("ryksuguskyr")).toBe("ryksuguskyr");
+    expect(encode("sægæs")).toBe("sægæs");
   });
 });
